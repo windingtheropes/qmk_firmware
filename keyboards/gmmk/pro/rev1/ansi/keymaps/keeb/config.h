ce72bc098e227f327923be7cd3bd4504f768bce5
Jack Anderson <windingtheropes@gmail.com>
2022-07-24 15:54:20 -0400

    add keeb firmware

---
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/keeb/config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+/* USB Device descriptor parameter */
+#define DEVICE_VER 0x0001
+#define VENDOR_ID 0x320F
+#define PRODUCT_ID 0x5044
+#define MANUFACTURER Glorious
+#define PRODUCT GMMK Pro
+
+#define RAW_USAGE_ID 0x61
+#define RAW_USAGE_PAGE 0xFF60
---
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/keeb/rules.mk
@@ -0,0 +1,3 @@
+RAW_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+DEBOUNCE_TYPE = sym_defer_pk
